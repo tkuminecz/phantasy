4dc1b56632896bc59a0fabdd2545926606ec2d82
Tim Kuminecz <tkuminecz@gmail.com>
2016-11-16 01:52:07 -0500

    implement task monad

---
+++ src/task.js
@@ -0,0 +1,98 @@
+// @flow
+import { Maybe } from '@/maybe';
+
+type TaskExecutor<A, X> = (res: (a: A) => void, rej: (x: X) => void) => void;
+
+/**
+ * The Task monad
+ */
+export class Task<A, X> {
+
+	runTask: TaskExecutor<A, X>
+
+	/**
+	 * Constructs a new Task instance
+	 */
+	constructor(runTask: TaskExecutor<A, X>) {
+		this.runTask = runTask;
+	}
+
+	/**
+	 * map :: Task a x ~> (a -> b) -> Task b x
+	 *
+	 * Transforms the result of this task if successful
+	 */
+	map<B>(f: (a: A) => B): Task<B, X> {
+		return new Task((succ, fail) => {
+			this.runTask(
+				val => succ(f(val)),
+				err => fail(err)
+			);
+		});
+	}
+
+	/**
+	 * andThen :: Task a x ~> (a -> Task b x) -> Task b x
+	 *
+	 * Passes the result of this task into another task-producing
+	 * function if successful
+	 */
+	andThen<B>(next: (a: A) => Task<B, X>): Task<B, X> {
+		return new Task((succ, fail) => {
+			this.runTask(
+				val => next(val).runTask(
+					val => succ(val),
+					err => fail(err)
+				),
+				err => fail(err)
+			);
+		});
+	}
+
+	/**
+	 * handleError :: Task a x ~> (x -> Task a x) -> Task a x
+	 *
+	 * Handles a failed task result
+	 */
+	handleError(handle: (x: X) => Task<A, *>): Task<A, *> {
+		return new Task((succ, fail) => {
+			this.runTask(
+				val => succ(val),
+				err => handle(err).runTask(
+					val => succ(val),
+					err => fail(err)
+				)
+			)
+		});
+	}
+
+	/**
+	 * toMaybe :: Task a x ~> () -> Task (Maybe a) x
+	 *
+	 * Transforms the task into a task that always
+	 * succeeds with a Maybe
+	 */
+	toMaybe(): Task<Maybe<A>, *> {
+		return this.andThen(a => Task.Success(Maybe.Just(a)))
+			.handleError(() => Task.Success(Maybe.Nothing()));
+	}
+
+	/**
+	 * Success :: a -> Task a x
+	 *
+	 * Returns a Task that always succeeds with the given value
+	 */
+	static Success(a: A): Task<A, *> {
+		return new Task(succ => succ(a));
+	}
+
+	/**
+	 * Fail :: x -> Task a x
+	 *
+	 * Returns a Task that always fails with the given value
+	 */
+	static Fail(x: X): Task<*, X> {
+		return new Task((_, fail) => fail(x));
+	}
+
+}
