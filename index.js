44336beba9a4729c2bcac017da3ac3e5f3ca6ca2
Tim Kuminecz <tkuminecz@gmail.com>
2016-11-18 17:13:56 -0500

    trying to fix flow issues

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // @flow
-module.exports = require('./lib/main');
+module.exports = {
+	IO: require('./lib/io'),
+	Maybe: require('./lib/maybe'),
+	Reader: require('./lib/reader'),
+	State: require('./lib/state'),
+	Task: require('./lib/task'),
+	Writer: require('./lib/writer')
+};
